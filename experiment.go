71124e6254d32cbd7f0509d8b9f479e90b62d1da
David R. Richardson <drr@drrlabs.com>
2012-09-10 00:07:31 +0000

    Simple experiements with go

--- experiment.go
+++ experiment.go
@@ -1,18 +1,51 @@
+/* A little fiddline around with Go
+ *
+ * Install a couple different http handlers
+ * Try some reflection
+ * Try some json encoding
+ */
+
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
+	"reflect"
+        "encoding/json"
+        "os"
+        "log"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-  fmt.Fprintln(w, "Hello World!")
-  fmt.Println("just hello'd", req)
+func rootdoc(w http.ResponseWriter, req *http.Request) {
+        dump(req)
+	fmt.Fprintln(w, "Hello World!")
+}
+
+func jsondoc(w http.ResponseWriter, req *http.Request) {
+        enc := json.NewEncoder(w)
+        if err := enc.Encode(req); err != nil {
+            log.Println(err)
+        }
+        b, err := json.MarshalIndent(req, "", "  ")
+        if err != nil {
+            log.Println(err)
+        }
+        os.Stdout.Write(b)
+}
+
+func dump(obj interface{}) {
+        v := reflect.ValueOf(obj)
+
+	fmt.Println("type: ", reflect.TypeOf(obj))
+	fmt.Println("value: ", v)
+	fmt.Println("kind: ", v.Kind())
+        fmt.Println("is struct? ", v.Kind() == reflect.Struct)
 }
 
 func main() {
-  http.HandleFunc("/", hello)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-      panic(err)
-  }
+	http.HandleFunc("/", rootdoc)
+	http.HandleFunc("/json", jsondoc)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
